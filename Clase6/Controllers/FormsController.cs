c12f1e853e5b9682e85f450ff98b71d4817c8001
Dxnale <daniel.torrealba@alumnos.ipleones.cl>
2024-11-30 00:51:53 -0300

    Agregar archivos de proyecto.

---
+++ Clase6/Controllers/FormsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase6.Controllers
+{
+    public class FormsController : Controller
+    {
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        public IActionResult SaveForm(string nom, string edad, string mail)
+        {
+            ViewBag.respuesta = nom;
+            ViewBag.texto = "Respuesta a " + mail;
+            return View("/Views/Forms/SaveForm.cshtml");
+        }
+    }
+}
---
+++ Clase6/Controllers/TallerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase6.Controllers
+{
+    public class TallerController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Vista2()
+        {
+            return View();
+        }
+
+        public IActionResult Vista4() {
+
+            return View();
+        }
+    }
+}
