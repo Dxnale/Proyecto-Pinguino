09619567fb402d4f634f048c2cb0eca3fdb003b9
Epic <byepic@outlook.es>
2024-12-04 20:46:36 -0300

    Diseño page de ventas

---
+++ Clase6/Controllers/FunctionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase6.Controllers
+{
+    public class FunctionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Venta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Venta(string txtproducto, int txtcantidad, double txtprecio)
+        {
+ 
+                ViewBag.producto = txtproducto;
+                ViewBag.cantidad = txtcantidad;
+                ViewBag.precio = txtprecio;
+                return View("/Views/Functions/Venta.cshtml");
+        }
+    }
+}
