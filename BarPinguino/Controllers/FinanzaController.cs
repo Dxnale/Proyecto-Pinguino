38183d458bc194c67e7ccfc0a4a047f6f480a1f3
Dxnale <daniel.torrealba@alumnos.ipleones.cl>
2024-12-07 18:30:07 -0300

    controler creation

---
+++ BarPinguino/Controllers/FinanzaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVA2TI_BarPinguino.Controllers
+{
+    public class FinanzaController : Controller
+    {
+        public IActionResult Decuentos()
+        {
+            return View();
+        }
+        public IActionResult Insumos()
+        {
+            return View();
+        }
+        public IActionResult Proveedores()
+        {
+            return View();
+        }
+        public IActionResult Stock()
+        {
+            return View();
+        }
+        public IActionResult VentasDia()
+        {
+            return View();
+        }
+        public IActionResult VentasMes()
+        {
+            return View();
+        }
+    }
+}
