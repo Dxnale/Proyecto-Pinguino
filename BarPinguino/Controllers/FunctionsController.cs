a496f1cb1c930165954e9e1d01372aa192b2aaa4
Benjamin Sequeida <benjaminsequeida95@gmail.com>
2024-12-10 19:08:29 -0300

    inventario,preparacion procedimiento

---
+++ BarPinguino/Controllers/FunctionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVA2TI_BarPinguino.Controllers
+{
+    public class FunctionsController : Controller
+    {
+        public IActionResult Preparacion()
+        {
+            return View("/Views/Functions/Preparacion.cshtml");
+        }
+        public IActionResult Procedimiento()
+        {
+            return View("/Views/Functions/Procedimiento.cshtml");
+        }
+        public IActionResult Inventario()
+        {
+            return View("/Views/Functions/Inventario.cshtml");
+        }
+        public IActionResult Restock()
+        {
+            return View("/Views/Functions/Restock.cshtml");
+        }
+        public IActionResult CorreoProveedores()
+        {
+            return View("/Views/Functions/CorreoProveedores.cshtml");
+        }
+    }
+}
