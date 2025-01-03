db11e522792836f01bbf69bb1402dfbfed71895b
Epic <byepic@outlook.es>
2025-01-02 21:33:58 -0300

    Inicio, Contexto bdd

---
+++ BarPinguino/Data/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EVA2TI_BarPinguino.Models;
+
+namespace EVA2TI_BarPinguino.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+        }
+        public DbSet<Clientes> Clientes { get; set; }
+
+        private void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BarPinguino/Models/Clientes.cs
@@ -0,0 +1,10 @@
+﻿namespace EVA2TI_BarPinguino.Models
+{
+    public class Clientes
+    {
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Frecuente { get; set; }
+    }
+}
