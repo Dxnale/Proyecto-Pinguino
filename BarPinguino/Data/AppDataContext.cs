87cf3b54582bee2a84615958a5d1b3b870c3ac15
Epic <byepic@outlook.es>
2025-01-05 21:52:56 -0300

    Creadobuilder

--- BarPinguino/Data/AppDataContext.cs
+++ BarPinguino/Data/AppDataContext.cs
@@ -11,10 +11,14 @@ namespace EVA2TI_BarPinguino.Data
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Usuarios> Usuarios { get; set; }
         public DbSet<Venta> Venta { get; set; }
+        public DbSet<Descuentos> Descuentos { get; set; }
+        public DbSet<Finanzas> Finanzas { get; set; }
+        public DbSet<Proveedores> Proveedores { get; set; }
+        public DbSet<Stock> Stock { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Clientes>(tb => 
+            modelBuilder.Entity<Clientes>(tb =>
             {
                 tb.HasKey(c => c.Rut);
 
@@ -26,6 +30,92 @@ namespace EVA2TI_BarPinguino.Data
             });
 
             modelBuilder.Entity<Clientes>().ToTable("Clientes");
+
+            modelBuilder.Entity<Usuarios>(tb =>
+            {
+                tb.HasKey(u => u.Credencial);
+
+                tb.Property(u => u.Credencial).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(u => u.clave).HasMaxLength(50).IsRequired();
+                tb.Property(u => u.Nombre).HasMaxLength(50).IsRequired();
+                tb.Property(u => u.TipoDeUsuario).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<Usuarios>().ToTable("Usuarios");
+
+            modelBuilder.Entity<Venta>(tb =>
+            {
+                tb.HasKey(v => v.Num_Boleta);
+
+                tb.Property(v => v.Num_Boleta).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(v => v.Credencial).IsRequired();
+                tb.Property(v => v.Detalles).HasMaxLength(200).IsRequired();
+                tb.Property(v => v.cliente_rut).IsRequired();
+                tb.Property(v => v.TotalPedido).IsRequired();
+            });
+
+            modelBuilder.Entity<Venta>().ToTable("Venta");
+
+            modelBuilder.Entity<Descuentos>(tb =>
+            {
+                tb.HasKey(d => d.Sku);
+
+                tb.Property(d => d.Sku).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(d => d.Precio_original).HasMaxLength(50).IsRequired();
+                tb.Property(d => d.Precio_descuento).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<Descuentos>().ToTable("Descuentos");
+
+            modelBuilder.Entity<Finanzas>(tb =>
+            {
+                tb.HasKey(f => f.Informe_stock);
+
+                tb.Property(f => f.Informe_stock).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(f => f.Fecha).IsRequired();
+                tb.Property(f => f.Gasto).IsRequired();
+                tb.Property(f => f.ingreso).HasMaxLength(50).IsRequired();
+                tb.Property(f => f.Detalles).HasMaxLength(200).IsRequired();
+                tb.Property(f => f.n_documento).HasMaxLength(50).IsRequired();
+                tb.Property(f => f.tipo_documento).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<Finanzas>().ToTable("Finanzas");
+
+            modelBuilder.Entity<Proveedores>(tb =>
+            {
+                tb.HasKey(p => p.Rut);
+
+                tb.Property(p => p.Rut).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(p => p.Giro).HasMaxLength(50).IsRequired();
+                tb.Property(p => p.razon_social).HasMaxLength(100).IsRequired();
+                tb.Property(p => p.datos_bancarios).HasMaxLength(100).IsRequired();
+                tb.Property(p => p.Fono).IsRequired();
+                tb.Property(p => p.direccion).HasMaxLength(100).IsRequired();
+            });
+
+            modelBuilder.Entity<Proveedores>().ToTable("Proveedores");
+
+            modelBuilder.Entity<Stock>(tb =>
+            {
+                tb.HasKey(s => s.SKu);
+
+                tb.Property(s => s.SKu).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                tb.Property(s => s.Provedor).HasMaxLength(50).IsRequired();
+                tb.Property(s => s.stock).IsRequired();
+                tb.Property(s => s.Stock_critico).IsRequired();
+                tb.Property(s => s.precio).IsRequired();
+                tb.Property(s => s.Informe_stock).HasMaxLength(50).IsRequired();
+            });
+
+            modelBuilder.Entity<Stock>().ToTable("Stock");
         }
+
     }
 }
