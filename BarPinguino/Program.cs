43528f81959ad13c90658ba2f94f42d99a7ba205
Epic <byepic@outlook.es>
2025-01-05 21:19:35 -0300

    Configuracion appsettings, mas buidel

--- BarPinguino/Program.cs
+++ BarPinguino/Program.cs
@@ -1,7 +1,15 @@
+using EVA2TI_BarPinguino.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnec"));
+});
 
 var app = builder.Build();
 
