28e268021daaad12a626055ba3b2c2581a46be70
Epic <byepic@outlook.es>
2025-01-13 13:23:26 -0300

    Cambios de controllador para registros en total

--- BarPinguino/Controllers/RegistrosController.cs
+++ BarPinguino/Controllers/RegistrosController.cs
@@ -32,6 +32,47 @@ namespace EVA2TI_BarPinguino.Controllers
 
             return View("UserRegistrado", usuario);
         }
+        [HttpGet]
+        public IActionResult RegistrarUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registrado(Clientes clientes)
+        {
+
+            var newuser = new Clientes
+            {
+                Rut = clientes.Rut,
+                Nombre = clientes.Nombre,
+                Apellido = clientes.Apellido,
+                Frecuente = clientes.Frecuente
+            };
+            _context.Clientes.Add(newuser);
+            _context.SaveChanges();
+
+
+            return View("Registrado", clientes);
+        }
+
+        [HttpGet]
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Consulta(string txtRut)
+        {
+            var cl = _context.Clientes.FirstOrDefault(c => c.Rut == txtRut);
+            if (cl != null)
+            {
+                ViewBag.ShowModal = true;
+                return View(cl);
+            }
+
+            return View();
+        }
+        
 
     }
 }
