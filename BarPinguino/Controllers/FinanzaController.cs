a0892db2f43a9ef32fb51efbc1e470bd9d19633c
Epic <byepic@outlook.es>
2025-01-14 16:41:24 -0300

    validacion de cookies

--- BarPinguino/Controllers/FinanzaController.cs
+++ BarPinguino/Controllers/FinanzaController.cs
@@ -1,29 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EVA2TI_BarPinguino.Controllers
 {
     public class FinanzaController : Controller
     {
+        [Authorize]
         public IActionResult Descuentos()
         {
             return View();
         }
+        [Authorize]
         public IActionResult Insumos()
         {
             return View();
         }
+        [Authorize]
         public IActionResult Proveedores()
         {
             return View();
         }
+        [Authorize]
         public IActionResult Stock()
         {
             return View();
         }
+        [Authorize]
         public IActionResult VentasDia()
         {
             return View();
         }
+        [Authorize]
         public IActionResult VentasMes()
         {
             return View();
--- BarPinguino/Controllers/VentasController.cs
+++ BarPinguino/Controllers/VentasController.cs
@@ -3,6 +3,7 @@ using EVA2TI_BarPinguino.Models;
 using EVA2TI_BarPinguino.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace EVA2TI_BarPinguino.Controllers
@@ -15,17 +16,20 @@ namespace EVA2TI_BarPinguino.Controllers
         {
             _context = context;
         }
+        [Authorize]
         [HttpGet]
         public IActionResult Venta()
         {
             return View();
         }
+        [Authorize]
         [HttpPost]
         public IActionResult Venta(string txtproducto, int txtcantidad, double txtprecio)
         {
             var credencial = User.FindFirst("Credencial_Vendedor")!.Value;
             return View("/Views/Ventas/Venta.cshtml");
         }
+        [Authorize]
         public IActionResult Boleta()
         {
             return View();
