0a232215028db2d5035b48d0c6d2cd3666cab216
Epic <byepic@outlook.es>
2025-01-17 22:45:30 -0300

    Cambio sintexis de modelos

--- BarPinguino/Models/Descuentos.cs
+++ BarPinguino/Models/Descuentos.cs
@@ -1,18 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EVA2TI_BarPinguino.Models
 {
     public class Descuentos
     {
+        private int _sku;
+        private string _precioOriginal;
+        private string _precioConDescuento;
+        private Stock _stock;
+
         [Key]
-        public int SKU { get; set; }
+        public int SKU
+        {
+            get => _sku;
+            set => _sku = value;
+        }
 
-        public string PrecioOriginal { get; set; }
+        public string PrecioOriginal
+        {
+            get => _precioOriginal;
+            set => _precioOriginal = value;
+        }
 
-        public string PrecioConDescuento { get; set; }
+        public string PrecioConDescuento
+        {
+            get => _precioConDescuento;
+            set => _precioConDescuento = value;
+        }
 
         [ForeignKey("SKU")]
-        public Stock Stock { get; set; }
+        public Stock Stock
+        {
+            get => _stock;
+            set => _stock = value;
+        }
     }
 }
--- BarPinguino/Models/Finanzas.cs
+++ BarPinguino/Models/Finanzas.cs
@@ -4,19 +4,55 @@ namespace EVA2TI_BarPinguino.Models
 {
     public class Finanzas
     {
+        private string _informeDeStock;
+        private string _fecha;
+        private decimal _gasto;
+        private string _ingreso;
+        private string _detalles;
+        private string _nDocumento;
+        private string _tipoDeDocumento;
+
         [Key]
-        public string InformeDeStock { get; set; }
+        public string InformeDeStock
+        {
+            get => _informeDeStock;
+            set => _informeDeStock = value;
+        }
 
-        public string Fecha { get; set; }
+        public string Fecha
+        {
+            get => _fecha;
+            set => _fecha = value;
+        }
 
-        public decimal Gasto { get; set; }
+        public decimal Gasto
+        {
+            get => _gasto;
+            set => _gasto = value;
+        }
 
-        public string Ingreso { get; set; }
+        public string Ingreso
+        {
+            get => _ingreso;
+            set => _ingreso = value;
+        }
 
-        public string Detalles { get; set; }
+        public string Detalles
+        {
+            get => _detalles;
+            set => _detalles = value;
+        }
 
-        public string NDocumento { get; set; }
+        public string NDocumento
+        {
+            get => _nDocumento;
+            set => _nDocumento = value;
+        }
 
-        public string TipoDeDocumento { get; set; }
+        public string TipoDeDocumento
+        {
+            get => _tipoDeDocumento;
+            set => _tipoDeDocumento = value;
+        }
     }
 }
