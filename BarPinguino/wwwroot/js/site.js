3f6d76eab2e6a28078d2503f07dd334cc2585be4
BenjSeq <Benjaminsequeida95@gmail.com>
2025-01-19 03:05:48 -0300

    DobleFactor listo, falta recuperacion de claves y js

--- BarPinguino/wwwroot/js/site.js
+++ BarPinguino/wwwroot/js/site.js
@@ -1,1 +1,22 @@
-﻿
+﻿function DF() {
+    const form = document.getElementById('Mail');
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+    const formdos = document.getElementById('Maildos');
+    formdos.style.display = formdos.style.display === 'none' ? 'block' : 'none';
+    const recu = document.getElementById('Recu');
+    if (recu.style.display === 'block') {
+        recu.style.display = 'none';  // Si "Recu" está visible, lo ocultamos
+    } else {
+        recu.style.display = 'none';  // Si "Recu" está oculto, se mantiene oculto
+    }
+}
+function RC() {
+    const form = document.getElementById('Recu');
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+    const recu = document.getElementById('Mail');
+    if (recu.style.display === 'block') {
+        recu.style.display = 'none';  // Si "Recu" está visible, lo ocultamos
+    } else {
+        recu.style.display = 'none';  // Si "Recu" está oculto, se mantiene oculto
+    }
+}
